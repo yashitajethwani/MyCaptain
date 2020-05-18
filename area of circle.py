8afff30e54bcf8cf2cfb7df41f8813b989e3ba4a
yashitajethwani <65553538+yashitajethwani@users.noreply.github.com>
2020-05-18 20:38:23 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,10 @@
+r=float(input("Input the radius of the circle: "))
+print("The area of the circle with radius" , r , "is: ")
+print(22/7*r**2)
+
+a=input("Input the Filename: ")
+print("The extention of the file is: ")
+if a=="abc.py":
+    print('python')
+else:
+    print("not defined")
