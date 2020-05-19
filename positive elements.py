af77d53d9dcc5485ae197263f9accd754173d9df
yashitajethwani <65553538+yashitajethwani@users.noreply.github.com>
2020-05-19 23:07:05 +0530

    Add files via upload

---
+++ positive elements.py
@@ -0,0 +1,13 @@
+l1=[12,-7,5,64,-14]
+print("output L1: ")
+for i in l1:
+    if i>0:
+        print(i)
+print()
+        
+l2=[12,14,-95,3]
+print("output L2: ")
+for i in l2:
+    if i>0:
+        print(i)
+        
